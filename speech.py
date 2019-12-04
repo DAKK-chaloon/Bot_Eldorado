ff254d100873fc21e9bec327cdc18d32a995e174
DavidCC <davidghe@outlook.fr>
2019-12-04 15:31:14 +0100

    Test of speechtotalk

--- speech.py
+++ speech.py
@@ -28,7 +28,9 @@ def convertToAudio(textToConvert):
     response = client.synthesize_speech(synthesis_input, voice, audio_config)
 
     # The response's audio_content is binary.
-    with open('output.mp3', 'wb') as out:
+    with open('output.wav', 'wb') as out:
         # Write the response to the output file.
         out.write(response.audio_content)
         print('Audio content written to file "output.mp3"')
+
+convertToAudio("Bienvenue à tous !")
